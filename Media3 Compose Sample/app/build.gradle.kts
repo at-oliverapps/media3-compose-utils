23ec244193b60c1fb030019dcd2988674ae9c848
Oliver Lauritsen <81589599+at-oliverapps@users.noreply.github.com>
2025-11-19 06:26:37 +0100

    Remove unused test dependencies
    Removed test dependencies from build.gradle.kts

--- Media3 Compose Sample/app/build.gradle.kts
+++ Media3 Compose Sample/app/build.gradle.kts
@@ -18,8 +18,6 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -66,11 +64,6 @@ dependencies {
     implementation(libs.media3.database)
     implementation(libs.media3.ui.compose)
     implementation(libs.coil.compose)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
