608dbf7687e2e6b71eb26b6b8baf858496a0c397
Oliver Lauritsen <81589599+at-oliverapps@users.noreply.github.com>
2026-07-06 11:17:00 +0200

    Update SeekBackButtonState to handle nullable Player
    Added optional player to be in line with media3 compose other remember functions

--- Media3ComposeSample/shared/src/main/java/io/oliverapps/media3/compose/shared/ui/state/SeekBackButtonState.kt
+++ Media3ComposeSample/shared/src/main/java/io/oliverapps/media3/compose/shared/ui/state/SeekBackButtonState.kt
@@ -8,45 +8,39 @@ import androidx.compose.runtime.*
 import androidx.media3.common.Player
 import androidx.media3.common.listen
 import androidx.media3.common.util.UnstableApi
+import androidx.media3.ui.compose.state.PlayerStateObserver
+import androidx.media3.ui.compose.state.observeState
 
 @UnstableApi
 @Composable
-fun rememberSeekBackButtonState(player: Player): SeekBackButtonState {
+fun rememberSeekBackButtonState(player: Player?): SeekBackButtonState {
     val seekBackButtonState = remember(player) { SeekBackButtonState(player) }
     LaunchedEffect(player) { seekBackButtonState.observe() }
     return seekBackButtonState
 }
 
 @UnstableApi
-class SeekBackButtonState(private val player: Player) {
+class SeekBackButtonState(private val player: Player?) {
 
-    var isEnabled by mutableStateOf(isSeekBackEnabled(player))
+    var isEnabled by mutableStateOf(false)
         private set
 
-    var seekBackAmountMs by mutableLongStateOf(player.seekBackIncrement)
+    var seekBackAmountMs by mutableLongStateOf(0)
         private set
 
     fun onClick() {
-        player.seekBack()
+        player?.seekBack()
     }
 
-    suspend fun observe(): Nothing {
+    private val playerStateObserver: PlayerStateObserver? = player?.observeState( Player.EVENT_TIMELINE_CHANGED, Player.EVENT_SEEK_BACK_INCREMENT_CHANGED) {
+        isEnabled = isSeekBackEnabled(it)
+        seekBackAmountMs = it.seekBackIncrement
+    }
 
-        isEnabled = isSeekBackEnabled(player)
-        seekBackAmountMs = player.seekBackIncrement
-
-        player.listen { events ->
-            if (
-                events.containsAny(
-                    Player.EVENT_TIMELINE_CHANGED,
-                    Player.EVENT_SEEK_BACK_INCREMENT_CHANGED,
-                )
-            ) {
-                isEnabled = isSeekBackEnabled(this)
-                seekBackAmountMs = seekBackIncrement
-            }
-        }
+    suspend fun observe() {
+        playerStateObserver?.observe()
     }
 
     private fun isSeekBackEnabled(player: Player) = player.isCurrentMediaItemDynamic
+
 }
