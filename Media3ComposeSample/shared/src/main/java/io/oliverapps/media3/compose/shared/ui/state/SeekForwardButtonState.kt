470e34b61bfa86aa614205992cb0913a234e32e6
Oliver Lauritsen <81589599+at-oliverapps@users.noreply.github.com>
2026-07-06 11:18:55 +0200

    Update SeekForwardButtonState for nullable Player
    added optional player to be in line with media3 compose other remember functions

--- Media3ComposeSample/shared/src/main/java/io/oliverapps/media3/compose/shared/ui/state/SeekForwardButtonState.kt
+++ Media3ComposeSample/shared/src/main/java/io/oliverapps/media3/compose/shared/ui/state/SeekForwardButtonState.kt
@@ -8,47 +8,39 @@ import androidx.compose.runtime.*
 import androidx.media3.common.Player
 import androidx.media3.common.listen
 import androidx.media3.common.util.UnstableApi
+import androidx.media3.ui.compose.state.PlayerStateObserver
+import androidx.media3.ui.compose.state.observeState
 
 @UnstableApi
 @Composable
-fun rememberSeekForwardButtonState(player: Player): SeekForwardButtonState {
+fun rememberSeekForwardButtonState(player: Player?): SeekForwardButtonState {
     val seekForwardButtonState = remember(player) { SeekForwardButtonState(player) }
     LaunchedEffect(player) { seekForwardButtonState.observe() }
     return seekForwardButtonState
 }
 
 @UnstableApi
-class SeekForwardButtonState(private val player: Player) {
+class SeekForwardButtonState(private val player: Player?) {
 
-    var isEnabled by mutableStateOf(isSeekForwardEnabled(player))
+    var isEnabled by mutableStateOf(false)
         private set
 
-    var seekForwardAmountMs by mutableLongStateOf(player.seekForwardIncrement)
+    var seekForwardAmountMs by mutableLongStateOf(0)
         private set
 
     fun onClick() {
-        player.seekForward()
+        player?.seekForward()
     }
 
-    suspend fun observe(): Nothing {
-
-        isEnabled = isSeekForwardEnabled(player)
-        seekForwardAmountMs = player.seekForwardIncrement
-
-        player.listen { events ->
-            if (
-                events.containsAny(
-                    Player.EVENT_TIMELINE_CHANGED,
-                    Player.EVENT_SEEK_FORWARD_INCREMENT_CHANGED,
-                )
-            ) {
-                isEnabled = isSeekForwardEnabled(this)
-                seekForwardAmountMs = seekForwardIncrement
-            }
-        }
+    private val playerStateObserver: PlayerStateObserver? = player?.observeState( Player.EVENT_TIMELINE_CHANGED, Player.EVENT_SEEK_BACK_INCREMENT_CHANGED) {
+        isEnabled = isSeekForwardEnabled(it)
+        seekForwardAmountMs = it.seekForwardIncrement
     }
 
-    private fun isSeekForwardEnabled(player: Player) =
-        player.isCurrentMediaItemDynamic
+    suspend fun observe() {
+        playerStateObserver?.observe()
+    }
+
+    private fun isSeekForwardEnabled(player: Player) = player.isCurrentMediaItemDynamic
 
 }
